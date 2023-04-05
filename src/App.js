8fdcfa5bcacc73ee5b8764f2593cf686a6e8b968
SanjuManna <sanjumanna2201@gmail.com>
2023-04-05 23:39:49 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Quora from './components/quora/Quora';
 import { useSelector } from 'react-redux'
 import { selectUser } from './features/userSlice';
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import ForgotPassword from './components/auth/ForgotPassword';
+let dispatchFun = {};
+const initialState = { status: "login" };
 
-
-
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "register":
+      return { status: "register" };
+    case "login":
+      return { status: "login" };
+    case "logout":
+      return { status: "logout" };
+    case "forgotPassword":
+      return { status: "forgotPassword" };
+    default:
+      return initialState;
+  }
+};
 
 function App() {
   const user = useSelector(selectUser);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  dispatchFun = { dispatch }
   return (
     <div className="App">
-      {/* {user ? (<Quora />) : (<Login />)} */}
-      <Quora />
-
+      <BrowserRouter>
+        <Routes>
+          <Route >
+            <Route path='/' element={<Quora />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgotPassword" element={<ForgotPassword />} />
+            {/* <Route path="/profile">
+              <Route path=":userId" element={<Profile />} />
+              <Route path=":userId/edit" element={<EditProfile />} />
+            </Route> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
+export { dispatchFun };
 export default App;
---
+++ src/components/auth/firebase.js
@@ -0,0 +1,113 @@
+
+
+import { initializeApp } from "firebase/app";
+import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+    signOut,
+    FacebookAuthProvider
+} from "firebase/auth";
+import {
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBewoXYyA6-F70ixHK_II5JT0Xq2jU7dyA",
+    authDomain: "quora-clone-68ab1.firebaseapp.com",
+    projectId: "quora-clone-68ab1",
+    storageBucket: "quora-clone-68ab1.appspot.com",
+    messagingSenderId: "438447826262",
+    appId: "1:438447826262:web:cbb5352eb05bdcb8e0a2f7",
+    measurementId: "G-9S9P8FVEGJ"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+
+const signInWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        return true;
+    } catch (err) {
+        alert(err.message);
+        return false;
+    }
+};
+const signInWithFacebook = async () => {
+    try {
+        const res = await signInWithPopup(auth, facebookProvider);
+        return true;
+    } catch (err) {
+        alert(err.message);
+        return false;
+    }
+};
+
+
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+        await printAuth();
+        return true;
+    } catch (err) {
+        alert(err.message);
+        return false;
+    }
+};
+
+
+const printAuth = () => {
+    console.log(auth)
+}
+
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        alert(err.message);
+    }
+};
+
+const sendPasswordReset = async (email) => {
+    try {
+        await sendPasswordResetEmail(auth, email);
+        alert("Password reset link sent!");
+        return true;
+    } catch (err) {
+        alert("Email not found, Please check your email.");
+        return false;
+    }
+};
+
+
+const logout = () => {
+    signOut(auth);
+};
+
+
+export {
+    auth,
+    db,
+    signInWithGoogle,
+    signInWithFacebook,
+    logInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordReset,
+    logout,
+};
